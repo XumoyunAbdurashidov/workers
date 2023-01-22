90fbb669dc224278606b0d65ed67287b26beb756
Xumoyun <abdurashidovxumoyun4577@gmail.com>
2023-01-22 07:47:44 +0500

    DomContentLoaded

--- js/history.js
+++ js/history.js
@@ -1,20 +1,18 @@
 "use stict";
 
-const btn = document.querySelector("#btn__");
-const box = document.querySelector(".id_js");
+document.addEventListener("DOMContentLoaded", () => {
+  const btn = document.querySelector("#btn__");
+  const box = document.querySelector(".id_js");
 
-box.style.display = "none";
+  box.style.display = "none";
 
-btn.addEventListener("click", () => {
-  if (box.style.display == "none") {
-    box.style.display = "block";
-  } else {
-    box.style.display = "none";
-  }
-
-  //   if (box.style.display == "none") {
-  //     box.style.display = "block";
-  //   } else {
-  //     box.style.display = "none";
-  //   }
+  btn.addEventListener("click", () => {
+    if (box.style.display == "none") {
+      box.style.display = "block";
+      btn.style.transform = "rotate(180deg)";
+    } else {
+      box.style.display = "none";
+      btn.style.transform = "rotate(360deg)";
+    }
+  });
 });
--- js/profil.js
+++ js/profil.js
@@ -1,23 +1,25 @@
-function dropdownMenu() {
-  let container = document.querySelector(".container_box");
-  let drop = document.querySelector(".drop_box");
-  let btn = document.querySelector("#btn_");
-  let close = document.querySelector(".close");
+document.addEventListener("DOMContentLoaded", () => {
+  function dropdownMenu() {
+    let container = document.querySelector(".container_box");
+    let drop = document.querySelector(".drop_box");
+    let btn = document.querySelector("#btn_");
+    let close = document.querySelector(".close");
 
-  container.style.display = "none";
-
-  close.addEventListener("click", () => {
     container.style.display = "none";
-  });
 
-  btn.addEventListener("click", () => {
-    if (container.style.display == "none") {
-      container.style.display = "block";
-      drop.style.display = "block";
-    } else {
+    close.addEventListener("click", () => {
       container.style.display = "none";
-      drop.style.display = "none";
-    }
-  });
-}
-dropdownMenu();
+    });
+
+    btn.addEventListener("click", () => {
+      if (container.style.display == "none") {
+        container.style.display = "block";
+        drop.style.display = "block";
+      } else {
+        container.style.display = "none";
+        drop.style.display = "none";
+      }
+    });
+  }
+  dropdownMenu();
+});
